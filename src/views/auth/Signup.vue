089b901e6907fa1e1721ea1c405a98bb69b4071f
onikoko abdullahi <Onikokoabdullahi@gmail.com>
2021-04-07 15:16:38 +0100

    Implement signup form

--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -1,124 +1,199 @@
 <template>
   <div>
     <vue-headful title="Signup | Ava" description="Description goes here" />
-    <div class="row">
-      <div class="col-6 col-sm-12">
-        <div class="container">
-          <div class="form-block">
-            <h6 class="text-center">Welcome to Ava!</h6>
-            <h5 class="text-center font-weight-bold">
-              Let's kick off with few simple steps
-            </h5>
-            <span class="icon_arrow_down"></span>
-            <form action="">
-              <div class="form-group row d-flex">
-                <div class="col-md-6">
-                  <label
-                    for="firstName"
-                    class="color_grey_dark text-uppercase font-weight-bold font-13"
-                    >First name</label
-                  >
-                  <input
-                    type="text"
-                    id="firstName"
-                    class="form-control"
-                    placeholder="First name"
-                  />
+
+    <div class="signup-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6 col-lg-6 left-layout">
+            <div class="form-block">
+              <h6 class="text-center">Welcome to Ava!</h6>
+              <h5 class="text-center font-weight-bold">
+                Let's kick off with few simple steps
+              </h5>
+
+              <form action="">
+                <div class="row">
+                  <div class="col-12 col-md-6 col-lg-6">
+                    <div class="form-group">
+                      <label
+                        for="firstName"
+                        class="color_grey_dark font-weight-bold font-13"
+                        >First name</label
+                      >
+                      <input
+                        type="text"
+                        id="firstName"
+                        class="form-control"
+                        placeholder="First name"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-12 col-md-6 col-lg-6">
+                    <div class="form-group">
+                      <label
+                        for="lastName"
+                        class="color_grey_dark font-weight-bold font-13"
+                        >Last name</label
+                      >
+                      <input
+                        type="text"
+                        id="lastName"
+                        class="form-control"
+                        placeholder="Last name"
+                      />
+                    </div>
+                  </div>
                 </div>
 
-                <div class="col-md-6">
-                  <label
-                    for="lastName"
-                    class="color_grey_dark text-uppercase font-weight-bold font-13"
-                    >Last name</label
-                  >
-                  <input
-                    type="text"
-                    id="lastName"
-                    class="form-control"
-                    placeholder="Last name"
-                  />
+                <div class="row">
+                  <div class="col-12">
+                    <div class="form-group">
+                      <label
+                        for="email"
+                        class="color_grey_dark font-weight-bold font-13"
+                        >Work email address</label
+                      >
+                      <input
+                        type="email"
+                        class="form-control"
+                        required
+                        placeholder="example@mail.com"
+                      />
+                    </div>
+                  </div>
                 </div>
+
+                <div class="row">
+                  <div class="col-12 col-md-6 col-lg-6">
+                    <div class="form-group">
+                      <label
+                        for="password"
+                        class="color_grey_dark font-weight-bold font-13"
+                        >Password</label
+                      >
+                      <input
+                        type="password"
+                        id="password"
+                        class="form-control"
+                        placeholder="Password"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-12 col-md-6 col-lg-6">
+                    <div class="form-group">
+                      <label
+                        for="confrimPass"
+                        class="color_grey_dark font-weight-bold font-13"
+                        >Confirm passsword</label
+                      >
+                      <input
+                        type="password"
+                        id="confirmPass"
+                        class="form-control"
+                        placeholder="Confirm password"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-12 col-md-6 col-lg-6">
+                    <div class="form-group">
+                      <label
+                        for="company"
+                        class="color_grey_dark font-weight-bold font-13"
+                        >Company name</label
+                      >
+                      <input
+                        type="text"
+                        id="company"
+                        class="form-control"
+                        placeholder="Company name"
+                      />
+                    </div>
+                  </div>
+                  <div class="col-12 col-md-6 col-lg-6">
+                    <div class="form-group">
+                      <label
+                        for="industry"
+                        class="color_grey_dark font-weight-bold font-13"
+                        >Industry</label
+                      >
+                      <select required class="form-control">
+                        <option value selected disabled>Select Industry</option>
+                        <option>agric</option>
+                        <option>tech</option>
+                        <option>engi</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="auth-cta w-100 d-flex justify-content-center">
+                  <button class="btn btn-primary btn-stretch my-3">
+                    Register
+                  </button>
+                </div>
+                <!-- DISCLAIMER BLOCK -->
+                <div class="disclaimer-block color_grey_dark">
+                  <p class="text-center">
+                    By signing up, you agree to our
+                    <router-link :to="{ name: '' }" class="btn-link"
+                      >Privacy Policies</router-link
+                    >&nbsp;,&nbsp;
+                    <router-link :to="{ name: '' }" class="btn-link"
+                      >Terms of Use</router-link
+                    >&nbsp;and&nbsp;
+                    <router-link :to="{ name: '' }" class="btn-link"
+                      >Cookie Policy</router-link
+                    >
+                  </p>
+                </div>
+              </form>
+
+              <div class="auth-signup-cta mt-3">
+                <p class="text-center font-15">
+                  Already have an account?
+                  <router-link :to="{ name: 'login' }" class="btn-link"
+                    >&nbsp; Login here</router-link
+                  >
+                </p>
               </div>
+            </div>
 
-              <div class="form-group">
-                <label for="email" class="form-label">Work email address</label>
-                <input
-                  type="email"
-                  class="form-control"
-                  required
-                  placeholder="example@mail.com"
-                />
-              </div>
+            <!-- AUTH COPYRIGHT -->
+            <div class="auth-copyright mt-4 mb-3">
+              <p class="text-center color_grey_dark font-12">
+                © {{ currentDate.getFullYear() }}, Ava. All rights reserved.
+              </p>
+            </div>
+          </div>
 
-              <div class="form-group row d-flex">
-                <div class="col-md-6">
-                  <label
-                    for="password"
-                    class="color_grey_dark text-uppercase font-weight-bold font-13"
-                    >Password</label
-                  >
-                  <input
-                    type="password"
-                    id="password"
-                    class="form-control"
-                    placeholder="Password"
-                  />
-                </div>
-
-                <div class="col-md-6">
-                  <label
-                    for="confirmPass"
-                    class="color_grey_dark text-uppercase font-weight-bold font-13"
-                    >Confirm password</label
-                  >
-                  <input
-                    type="password"
-                    id="confirmPass"
-                    class="form-control"
-                    placeholder="Confirm password"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row d-flex">
-                <div class="col-md-6">
-                  <label
-                    for="company"
-                    class="color_grey_dark text-uppercase font-weight-bold font-13"
-                    >Company name</label
-                  >
-                  <input
-                    type="text"
-                    id="company"
-                    class="form-control"
-                    placeholder="Company name"
-                  />
-                </div>
-
-                <div class="col-md-6"></div>
-              </div>
-              <div class="btn btn-primary">Register</div>
-            </form>
+          <div class="col-12 col-md-6 col-lg-6 right-layout">
+            <h1 class="font-weight-bold">
+              Performance management has never been easier
+            </h1>
+            <img
+              src="../../../public/img/landing.svg"
+              alt="landing image"
+              class="img-responsive"
+            />
           </div>
         </div>
-      </div>
-      <div class="col-6 col-sm-12">
-        <h1 class="font-weight-bold">
-          Performance management has never been easier
-        </h1>
-        <img
-          src="../../../public/img/landing.svg"
-          alt="landing image"
-          class="img-responsive"
-        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      currentDate: new Date(),
+    };
+  },
+};
 </script>
 
 <style></style>
